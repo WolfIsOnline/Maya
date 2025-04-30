3bc4dd148b1c2c1fcf41002f2a1b63bd2b87e85f
WolfIsOnline <cmzpoison@gmail.com>
2025-04-30 07:22:08 -0400

    Added DISCORD_TOKEN & DEBUG_GUILD

--- frontend/maya/__init__.py
+++ frontend/maya/__init__.py
@@ -3,5 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-BACKEND_URL = os.environ.get("OWNER_ID")
+BACKEND_URL = os.environ.get("BACKEND_URL")
 OWNER_ID = os.environ.get("OWNER_ID")
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+DEBUG_GUILD = os.environ.get("DEBUG_GUILD")
