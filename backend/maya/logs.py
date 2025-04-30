bf0c0c38d43ea8b3c36bd0cfd6410e0a8e5bec8d
WolfIsOnline <cmzpoison@gmail.com>
2025-04-30 19:35:44 -0400

    logging formatter

---
+++ backend/maya/logs.py
@@ -0,0 +1,33 @@
+import logging
+from colorlog import ColoredFormatter
+
+LOG_LEVEL = logging.DEBUG
+
+
+def setup_logging():
+    """Setups custom logging formatting
+
+    Returns:
+        logger: logger object
+    """
+    logger = logging.getLogger("maya")
+    logger.setLevel(LOG_LEVEL)
+
+    handler = logging.StreamHandler()
+    formatter = ColoredFormatter(
+        "%(log_color)s%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bold_red",
+        },
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+log = setup_logging()
